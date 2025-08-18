1969d8d4fa014770087279ce93639629db5c2bdb
Caro <Carolina.Hoffmann@msg-life.com>
2025-08-18 19:34:29 +0200

    Update serializer and deserializer, json file

--- GamePlay/src/gameSettings.java
+++ GamePlay/src/gameSettings.java
@@ -5,5 +5,19 @@ public class gameSettings {
 
     public void gameStart() {
         System.out.println("Start\nChoose your character\n\nAre you a hunter or the hunted?");
+
+    }
+
+
+    //JSON zu JAVA(JSON Datei gelesen werden kann)
+    public String jsonDeserializer(){
+
+        return "";
+    }
+
+    //JAVA zu JSON (JSON Datei zu schreiben)
+    public String jsonSerializer(){
+
+        return "";
     }
 }
